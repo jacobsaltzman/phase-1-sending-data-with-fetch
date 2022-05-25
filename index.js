3078a0a373941b1be446f1381c882e6203d792f0
Jacob Saltzman <j.daniel.saltzman@gmail.com>
2022-05-24 20:26:28 -0700

    commit tree acde77b20843adb2d262043f0daf736e28c4d34a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+function submitData(name, email){
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name, 
+      email
+    })
+  })
+  .then(res => res.json())
+  .then(res => {
+    document.body.innerHTML = `<p>${res.id}</p>`
+  })
+  .catch(function (error) {
+    document.body.innerHTML = `<p>Unauthorized Access</P>`
+  })
+}
